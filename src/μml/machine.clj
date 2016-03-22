a187ffcec63b02019b7c72775ca68d88e926f8e1
zjhmale <zjhmale@gmail.com>
2016-03-23 00:00:40 +0800

    more machine operators

--- src/μml/machine.clj
+++ src/μml/machine.clj
@@ -1,5 +1,8 @@
 (ns μml.machine
-  (require [adt.sweet :refer :all]))
+  (require [adt.sweet :refer :all])
+  (:refer-clojure :exclude [pop]))
+
+(def throw-exp #(throw (Exception. %)))
 
 ;; machine values
 (defadt ::mvalue
@@ -46,7 +49,7 @@
 
 (defn lookup
   [x envs]
-  (let [throw-exp #(throw (Exception. (str "unknown " x)))]
+  (let [throw-exp #(throw-exp (str "unknown " x))]
     (match envs
       EmptyEnvrionment (throw-exp)
       (NameMValues envs) (let [[env & _] envs]
@@ -56,26 +59,55 @@
 (defn pop
   [stack]
   (match stack
-    EmptyStack (throw (Exception. "empty stack"))
+    EmptyStack (throw-exp "empty stack")
     (MValues mvalues) (let [[head & tail] mvalues]
-                        [head (vec tail)])))
+                        [head tail])))
 
 (defn pop-bool
   [stack]
-  (let [throw-exp #(throw (Exception. "bool expected"))]
+  (let [throw-exp #(throw-exp "bool expected")]
     (match stack
       EmptyStack (throw-exp)
       (MValues mvalues) (let [[head & tail] mvalues]
                           (match head
-                            (BoolM v) [head (vec tail)]
+                            (BoolM v) [head tail]
                             :else (throw-exp))))))
 
 (defn pop-app
   [stack]
-  (let [throw-exp #(throw (Exception. "value and closure expected"))]
+  (let [throw-exp #(throw-exp "value and closure expected")]
     (match stack
       EmptyStack (throw-exp)
-      (MValues mvalues) (let [[v closure & t]]
+      (MValues mvalues) (let [[v closure & t] mvalues]
                           (match closure
                             (ClosureT fn an e) [fn an e v t]
                             :else (throw-exp))))))
+
+;; arith operations take their arguments from a stack and put the result back onto the stack.
+
+(defn arith-op
+  [mvalue op op-name]
+  (fn [stack]
+    (let [throw-exp #(throw-exp (str "int and int expected in " op-name))]
+      (match stack
+        EmptyStack (throw-exp)
+        (MValues mvalues) (let [[x y & t] mvalues]
+                            (match x
+                              (mvalue v1) (match y
+                                          (mvalue v2) (cons (mvalue (op v1 v2)) t)
+                                          :else (throw-exp))
+                              :else (throw-exp)))))))
+
+(def mult (arith-op IntM * "mult"))
+(def add (arith-op IntM + "add"))
+(def sub (arith-op IntM - "sub"))
+(def equal (arith-op BoolM = "equal"))
+(def less (arith-op BoolM < "less"))
+
+(defn exec
+  "executes instruction in the given state [frames, stack, envs],
+   where frames is a list of frames,
+   stack is just a stack, a list of machine values,
+   envs is a list of environments,
+   The return value is a new state."
+  [instruction frames stack envs])
