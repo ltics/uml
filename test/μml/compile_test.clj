78bc9819d6fb730c0a9e5c113eb783ebe0b25852
zjhmale <zjhmale@gmail.com>
2016-03-23 10:14:13 +0800

    test single step eval1

---
+++ test/μml/compile_test.clj
@@ -0,0 +1,8 @@
+(ns μml.compile-test
+  (:require [acolfut.sweet :refer :all]
+            [μml.ast :refer :all]
+            [μml.compile :refer :all]))
+
+(deftest compile-test
+  (testing "test compile"
+    ))
--- test/μml/eval_test.clj
+++ test/μml/eval_test.clj
@@ -5,6 +5,21 @@
   (:refer-clojure :exclude [eval]))
 
 (deftest eval-test
+  (testing "test eval1"
+    (let [expr1 (Times (Times (Int 3) (Int 3)) (Int 3))
+          expr2 (Less (Times (Int 3) (Int 3)) (Int 33))
+          expr3 (If (Bool false) (Int 3) (Int 33))
+          expr4 (If expr2 (Int 3) (Int 33))
+          incfn (Fun "inc" "x" IntT IntT (Plus (Var "x") (Int 1)))
+          expr5 (Apply incfn (Int 2))
+          expr6 (Apply incfn expr1)]
+      (is= (eval1 expr1) (Times (Int 9) (Int 3)))
+      (is= (eval1 expr2) (Less (Int 9) (Int 33)))
+      (is= (eval1 expr3) (Int 33))
+      (is= (eval1 expr4) (If (Less (Int 9) (Int 33)) (Int 3) (Int 33)))
+      (is= (eval1 expr5) (Plus (Int 2) (Int 1)))
+      (is= (eval1 expr6) (Apply incfn (Times (Int 9) (Int 3))))
+      (is (thrown? Exception (eval1 incfn)))))
   (testing "test eval"
     (let [expr1 (Times (Times (Int 3) (Int 3)) (Int 3))
           expr2 (Less (Times (Int 3) (Int 3)) (Int 33))
