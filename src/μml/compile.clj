06a48a29000b8eb957437b6a66fccd7abf55e07a
zjhmale <zjhmale@gmail.com>
2016-03-23 10:53:27 +0800

    add compile test

--- src/μml/compile.clj
+++ src/μml/compile.clj
@@ -10,15 +10,15 @@
   (let [arith (fn [e1 e2 instruction]
                 `(~@(compile e1) ~@(compile e2) ~instruction))]
     (match expr
-      (Var x) `(~VarI ~x)
-      (Int v) `(~IntI ~v)
-      (Bool v) `(~BoolI ~v)
+      (Var x) `(~(VarI x))
+      (Int v) `(~(IntI v))
+      (Bool v) `(~(BoolI v))
       (Times e1 e2) (arith e1 e2 MultI)
       (Plus e1 e2) (arith e1 e2 AddI)
       (Minus e1 e2) (arith e1 e2 SubI)
       (Equal e1 e2) (arith e1 e2 EqualI)
       (Less e1 e2) (arith e1 e2 LessI)
       (If p c a) `(~@(compile p) ~(BranchI (compile c) (compile a)))
-      (Fun fn an at rt e) (let [body `(~@(compile e) PopEnvI)]
+      (Fun fn an at rt e) (let [body `(~@(compile e) ~PopEnvI)]
                             `(~(ClosureI fn an body)))
-      (Apply e1 e2) `(~@(compile e1) ~@(compile e2) CallI))))
+      (Apply e1 e2) `(~@(compile e1) ~@(compile e2) ~CallI))))
--- test/μml/compile_test.clj
+++ test/μml/compile_test.clj
@@ -1,8 +1,20 @@
 (ns μml.compile-test
   (:require [acolfut.sweet :refer :all]
             [μml.ast :refer :all]
-            [μml.compile :refer :all]))
+            [μml.machine :refer :all]
+            [μml.compile :refer :all])
+  (:refer-clojure :exclude [pop compile]))
 
 (deftest compile-test
   (testing "test compile"
-    ))
+    (let [expr1 (Fun "eq3" "x" IntT BoolT (Equal (Var "x") (Int 3)))
+          expr2 (Apply expr1 (Int 3))
+          expr3 (Plus (Times (Int 3) (Int 1)) (Int 2))]
+      (is= (compile expr1)
+           (list (ClosureI "eq3" "x" (list (VarI "x") (IntI 3) EqualI PopEnvI))))
+      (is= (compile expr2)
+           (list (ClosureI "eq3" "x" (list (VarI "x") (IntI 3) EqualI PopEnvI))
+                 (IntI 3)
+                 CallI))
+      (is= (compile expr3)
+           (list (IntI 3) (IntI 1) MultI (IntI 2) AddI)))))
